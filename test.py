e8bb3cf19ef8c9b243f1317d230a9d5752511d93
holelung <junho335276@gmail.com>
2024-04-02 13:58:16 +0900

    수정

--- test.py
+++ test.py
@@ -25,7 +25,7 @@ box.send_keys(Keys.ENTER)
 
 # 스크롤 전 이미지 수
 prev_img_count = 0
-
+last_height = driver.execute_script("return document.body.scrollHeight")
 
 
 # 결과 로딩 대기
@@ -42,32 +42,33 @@ urls = []
 while True:
     if prev_img_count > 1000:
         break
-    
+    #dimg_io0LZoqTG_fd2roPl-KI0Ao_85
     # 페이지 끝까지 스크롤
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(5)  # 새 이미지 로드를 기다림
+    time.sleep(2)  # 새 이미지 로드를 기다림
+    new_height = driver.execute_script("return document.body.scrollHeight") 
+    
+    images = driver.find_elements(By.CSS_SELECTOR, "img.YQ4gaf")
 
-    images = driver.find_elements(By.CSS_SELECTOR, "img.Q4LuWd")
     
-    
-    if len(images) == prev_img_count:  # 새 이미지가 로드되지 않으면 종료
+    if new_height == last_height:  # 새 이미지가 로드되지 않으면 종료
         try:
-            more_button = WebDriverWait(driver, 10).until(
+            more_button = WebDriverWait(driver, 5).until(
                 EC.element_to_be_clickable((By.XPATH, '//*[@id="islmp"]/div/div/div/div/div[1]/div[2]/div[2]/input'))
             )
             more_button.click()
              
-            time.sleep(5)
+            time.sleep(2)
         except Exception as e:
             print("더보기 버튼을 찾거나 클릭할 수 없습니다.", e)
             break
         
     else:
-        prev_img_count = len(images)
+        last_height = new_height ##last_height update
 
     # 새로 로드된 이미지 URL 수집
     for image in images:
-        url = image.get_attribute('src')
+        url = image.get_attribute('src') or image.get_attribute('data-src')
         if url and 'http' in url and url not in urls:  # 중복 확인
             urls.append(url)
 
@@ -78,3 +79,6 @@ for i, url in enumerate(urls):
     urllib.request.urlretrieve(url, f"./{search_term}/{search_term}_{i}.jpg")
 
 driver.close()
+
+
+
